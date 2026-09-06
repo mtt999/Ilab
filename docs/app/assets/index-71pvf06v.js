a5520f8f469d833c08e6f437273372571596ae23
mtt999 <motlagh@illinois.edu>
2026-09-05 22:00:39 -0500

    Landing page: remove demo hints

--- docs/app/assets/index-71pvf06v.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BS8PBqH-.js","assets/index-Di3YDzof.js","assets/index-DR3YcH6S.css"])))=>i.map(i=>d[i]);
-import{H as e,_ as o}from"./index-Di3YDzof.js";const _=e("Browser",{web:()=>o(()=>import("./web-BS8PBqH-.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{_ as Browser};
---
+++ docs/app/assets/index-D-engKrG.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CWvqJOv_.js","assets/index-k-IMxh-E.js","assets/index-DR3YcH6S.css"])))=>i.map(i=>d[i]);
+import{H as t,_ as p}from"./index-k-IMxh-E.js";const _=t("RateApp",{web:()=>p(()=>import("./web-CWvqJOv_.js"),__vite__mapDeps([0,1,2])).then(e=>new e.RateAppWeb)});export{_ as RateApp};
